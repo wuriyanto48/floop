426c3d65ea1776577998c747cf1f2b3fe717e20d
wuriyanto <wuriyanto48@yahoo.co.id>
2018-02-18 19:53:26 +0700

    fix typo

--- floop.go
+++ floop.go
@@ -12,19 +12,19 @@ type floop struct {
 	every time.Duration
 }
 
-//New function, create pointer of floop
+//New function, the floop constructor
 func New(every time.Duration) *floop {
 	return &floop{every: every}
 }
 
-//Start function, execute function given to paramter and stop after duration > current time
+//Start function, execute function given to parameter and stop after duration > current time
 func (f *floop) Start(task fTask, duration time.Duration) {
 	stop := f.exec(task)
 	<-time.After(duration)
 	close(stop)
 }
 
-//exec function, execute function concurrently, after it stop
+//exec function, execute function concurrently
 func (f *floop) exec(task fTask) chan bool {
 	stop := make(chan bool, 1)
 	tick := time.NewTicker(time.Second * f.every)
